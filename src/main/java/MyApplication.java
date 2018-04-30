08db36c7926e3f6ce1db3acd4d9c82fe49f0f4be
hskEvelin <lucas.greising@hs-kempten.de>
2018-04-30 09:54:34 +0200

    .

---
+++ src/main/java/MyApplication.java
@@ -0,0 +1,9 @@
+package main.java;
+
+import com.sun.jersey.api.core.PackagesResourceConfig;
+
+public class MyApplication extends PackagesResourceConfig{
+	public MyApplication(){
+		super ("main.java");
+	}
+}
